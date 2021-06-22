1c071d4921644933a4c1e1e87d1b50fac86cfb92
Akshat verma <44364597+akshatver14@users.noreply.github.com>
2021-06-23 01:34:26 +0530

    Add files via upload
    install pytube library using pip command
    pip install pytube

---
+++ youtube_video_downloader.py
@@ -0,0 +1,15 @@
+import os
+from pytube import YouTube
+
+def downloader(link):
+    url = YouTube(link)
+
+    os.chdir(os.path.join(os.path.expanduser('~'),'desktop'))
+
+    video = url.streams.first()
+
+    video.download()
+
+    print(video.title,"- DOWNLOADED succesfully")
+
+downloader("https://www.youtube.com/watch?v=PT2_F-1esPk")
