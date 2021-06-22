d4ea8285171c79beb12363ed3e31747265a9e7dd
Akshat verma <44364597+akshatver14@users.noreply.github.com>
2021-06-23 01:32:01 +0530

    Add files via upload

---
+++ insta_dp_downloader.py
@@ -0,0 +1,12 @@
+import os
+import instaloader
+
+def profile(uname):
+    instance = instaloader.Instaloader()
+
+    os.chdir(os.path.join(os.path.expanduser('~'),'desktop'))
+
+    return ( instance.download_profile(uname,profile_pic_only=True))
+
+
+profile('narendramodi')
